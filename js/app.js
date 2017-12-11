86d6cf6b0c528fd37d75084938d1e41591bd972c
Tim Filer <tim.j.filer@gmail.com>
2017-12-11 15:42:00 -0600

    Fixed errors

--- js/app.js
+++ js/app.js
@@ -11,13 +11,11 @@
 	function NarrowItDownController(MenuSearchService){
 		
 		var mySearch = this;
-		mySearch.searchTerm = '';
-		mySearch.items = '';
 		mySearch.empty = false; 
 
 		mySearch.searchItems = function () {
 			mySearch.items = [];
-			if(mySearch.searchTerm.length == 0){
+			if(mySearch.searchTerm == undefined || mySearch.searchTerm.length == 0){
 				mySearch.empty = true;
 			} else{
 				MenuSearchService.getMatchedMenuItems(mySearch.searchTerm)
@@ -51,13 +49,18 @@
 		return ddo;
 	};
 
+
+
 	MenuSearchService.$inject = ['$http'];
 	function MenuSearchService ($http) {
 
 		var service = this;
-		var foundItems = [];
+		
 
 		service.getMatchedMenuItems = function(searchTerm) {
+
+			var foundItems = [];
+
 			return $http({
 				method: "GET",
 				url: "https://davids-restaurant.herokuapp.com/menu_items.json"
@@ -65,7 +68,7 @@
 
 			    // process result and only keep items that match
 			    for (var j = 0; j < result.data.menu_items.length; j++){
-				    if (result.data.menu_items[j].name.toLowerCase().indexOf(searchTerm) !== -1) {
+				    if (result.data.menu_items[j].description.toLowerCase().indexOf(searchTerm) !== -1) {
 				    	foundItems.push( {name : result.data.menu_items[j].name, short_name : result.data.menu_items[j].short_name, description : result.data.menu_items[j].description})
 				    }
 			    }
